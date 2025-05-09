bcb2144d56499b270f6c1954426e5a5166ab66db
Suhaibvp <118332112+Suhaibvp@users.noreply.github.com>
2025-05-09 19:38:59 +0530

    Update VideoFolder.kt

--- app/src/main/java/com/example/multitoolx/model/VideoFolder.kt
+++ app/src/main/java/com/example/multitoolx/model/VideoFolder.kt
@@ -7,12 +7,12 @@ package com.example.multitoolx.model
  * @param folderName The display name of the folder.
  * @param videoCount The number of video files in the folder.
  * @param videoPaths An optional list of paths to the video files.
- *                   This can be used to show a preview of the videos in the folder.
+ *                   This is used to show a preview of the videos in the folder.
  *                   If not provided, it can be left empty or null.
  */
 data class VideoFolder(
     val folderPath: String,    // The absolute path of the video folder on the device.
-    val folderName: String,    // The user-friendly name of the folder (e.g., "Movies", "Music Videos").
+    val folderName: String,    // The name of the folder (e.g., "Movies", "Music Videos").
     val videoCount: Int,       // The total number of video files in the folder.
     val videoPaths: List<String>? = null // A nullable list of video file paths for preview purposes.
 )
